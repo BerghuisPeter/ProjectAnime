8883628c8fd1e9b40be69936682eaf7f6dd95713
BerghuisPeter <peter.berghuis_13@hotmail.com>
2016-04-13 21:04:04 +0200

    Ajoute de structure personalisé
    ajoute de structure et premier page

---
+++ application/views/pages/home.php
@@ -0,0 +1,23 @@
+<aside>
+    <div id="menu">
+        <form id="menu_filtre">
+            <div class="filtre">
+                <input type="checkbox" id="chkbox" value="filtre1" />
+                <label for="chkbox1">Filtre #1</label>
+            </div>
+            <div class="filtre">
+                <input type="checkbox" id="chkbox" value="filtre2" />
+                <label for="chkbox1">Filtre #2</label>
+            </div>
+            <div class="filtre">
+                <input type="checkbox" id="chkbox" value="filtre3" />
+                <label for="chkbox1">Filtre #3</label>
+            </div>
+            <div class="filtre">
+                <input type="checkbox" id="chkbox" value="filtre4" />
+                <label for="chkbox1">Filtre #4</label>
+            </div>
+            <input type="text" class="search" name="search" class="search" placeholder="Search">
+        </form>
+    </div>
+</aside>
---
+++ application/views/templates/header.php
@@ -0,0 +1,22 @@
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>ProjectAnime</title>
+        <link href="<?php echo base_url(); ?>src/css/theme.css" type="text/css" rel="stylesheet">
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+    <header>
+        <div id="logo">
+            <p>Project <strong>Anime</strong></p>
+        </div>
+        <div id="header_menu">
+            <div id="login">
+                Connexion
+                <i class="ion-chevron-down"></i>
+            </div>
+        </div>
+    </header>
