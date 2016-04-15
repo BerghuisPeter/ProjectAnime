1741e4bf2dc6895e9345598b00aee9a0b442a2bf
BerghuisPeter <peter.berghuis_13@hotmail.com>
2016-04-15 18:36:50 +0200

    jhv
    jhvg

--- application/controllers/News.php
+++ application/controllers/News.php
@@ -3,10 +3,6 @@ class News extends CI_Controller {
 
     public function __construct()
     {
-
-
-
-        
         parent::__construct();
         $this->load->model('news_model');
         $this->load->helper('url_helper');
