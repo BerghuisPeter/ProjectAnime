d6edbfc071fcca02b1780d4cf1ffe0e1fbfb8c95
peter berghuis <peter.berghuis_13@hotmail.com>
2018-02-20 00:14:47 +0100

    clean #1

--- application/config/form_validation.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-$config['error_prefix'] = '<div class="alert alert-danger" role="alert">';
-$config['error_suffix'] = '</div>';
--- application/views/connection/login_success.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-echo $this->session->userName;
-echo "</br>";
-echo $this->session->email;
-echo "</br>";
-echo $this->session->password;
-echo "</br>";
-echo $this->session->id;
